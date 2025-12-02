d049b9f6c2fd504e8862c31150228fb1f508180d
Javier Martinez <javiermartinezalvarez98@gmail.com>
2025-12-02 16:34:54 +0100

    chore: update values

--- create_matrix.py
+++ create_matrix.py
@@ -23,29 +23,16 @@ EXCLUDE = [
 
 LINUX_MATRIX = {
     "flash-attn-version": [
-        "2.6.3",
-        "2.7.4",
         "2.8.3",
     ],
     "python-version": [
-        "3.10",
-        "3.11",
         "3.12",
-        # "3.13"
     ],
     "torch-version": [
-        "2.5.1",
-        "2.6.0",
-        "2.7.1",
-        "2.8.0",
-        "2.9.1",
+        "2.9.0",
     ],
     "cuda-version": [
-        "12.4.1",
-        "12.6.3",
-        "12.8.1",
-        # "12.9.1",
-        "13.0.2",
+        "12.9.1",
     ],
 }
 
@@ -56,55 +43,15 @@ LINUX_SELF_HOSTED_MATRIX = {
     "cuda-version": ["12.8.1", "13.0.1"],
 }
 
-WINDOWS_MATRIX = {
-    "flash-attn-version": [
-        "2.6.3",
-        "2.7.4",
-        "2.8.3",
-    ],
-    "python-version": [
-        "3.10",
-        "3.11",
-        "3.12",
-        # "3.13",
-    ],
-    "torch-version": [
-        "2.5.1",
-        "2.6.0",
-        "2.7.1",
-        "2.8.0",
-        "2.9.1",
-    ],
-    "cuda-version": [
-        "12.4.1",
-        "12.6.3",
-        "12.8.1",
-        # "12.9.1",
-        "13.0.2",
-    ],
-}
-
-WINDOWS_CODEBUILD_MATRIX = {
-    "flash-attn-version": ["2.6.3", "2.7.4.post1", "2.8.3"],
-    "python-version": ["3.10", "3.11", "3.12", "3.13"],
-    "torch-version": ["2.9.1"],
-    "cuda-version": ["13.0.1"],
-}
-
 
 def main():
     print(
         json.dumps(
             {
                 "linux": LINUX_MATRIX,
-                # "linux": False,
-                # "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
-                "linux_self_hosted": False,
-                "windows": WINDOWS_MATRIX,
-                # "windows": False,
-                # "windows_code_build": WINDOWS_CODEBUILD_MATRIX,
-                "windows_code_build": False,
-                "exclude": EXCLUDE,
+                "linux_enabled": True,
+                "linux_self_hosted": LINUX_SELF_HOSTED_MATRIX,
+                "linux_self_hosted_enabled": False,
             }
         )
     )
